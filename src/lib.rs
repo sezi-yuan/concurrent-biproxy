f8f70daf89c8c93d719e3a18f1315ac2597c1f95
yuanyi <sezi.yuan1989@gmail.com>
2022-01-17 00:05:30 +0800

    first initial
    Signed-off-by: yuanyi <sezi.yuan1989@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,297 @@
+use std::{
+    collections::HashMap,
+    io::Cursor,
+    pin::Pin,
+    sync::Arc,
+    task::{Context, Poll},
+};
+
+use async_trait::async_trait;
+use bytes::{Buf, BytesMut};
+use std::future::Future;
+use thiserror::Error;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{
+        tcp::{OwnedReadHalf, OwnedWriteHalf},
+        TcpStream,
+    },
+    sync::{broadcast, oneshot, Mutex},
+};
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("{0}")]
+    Io(#[from] std::io::Error),
+    #[error("channel disconnected")]
+    Disconnect,
+}
+
+#[derive(Error, Debug)]
+pub enum PacketError {
+    #[error("")]
+    Incomplete,
+    #[error("{0}")]
+    Other(String),
+}
+
+#[async_trait]
+pub trait IdentifierPacket: Sized + Send + 'static {
+    fn id(&self) -> &str;
+    /// return Ok(None) if need more bytes
+    async fn from_slice(src: &mut Cursor<&[u8]>) -> Result<Option<Self>>;
+    fn into_bytes(self) -> Vec<u8>;
+}
+
+#[derive(Debug, Clone)]
+pub struct WriteConnection {
+    inner: Arc<Mutex<OwnedWriteHalf>>,
+}
+
+impl WriteConnection {
+    pub fn new(write: OwnedWriteHalf) -> Self {
+        Self {
+            inner: Arc::new(Mutex::new(write)),
+        }
+    }
+
+    pub async fn write<P: IdentifierPacket>(&self, packet: P) -> Result<()> {
+        let bytes: Vec<u8> = packet.into_bytes();
+        self.inner.lock().await.write_all(bytes.as_slice()).await?;
+        Ok(())
+    }
+}
+
+#[async_trait]
+pub trait PacketProcessor<P>: Sync + Send + Clone + 'static
+where
+    P: IdentifierPacket,
+{
+    async fn process(&self, packet: P);
+}
+
+pub struct ReadConnection<P, PP>
+where
+    P: IdentifierPacket,
+    PP: PacketProcessor<P>,
+{
+    identifer: String,
+    inner: OwnedReadHalf,
+    response_notify: Arc<Mutex<HashMap<String, oneshot::Sender<P>>>>,
+    processor: PP,
+    buffer: BytesMut,
+}
+
+impl<P, PP> ReadConnection<P, PP>
+where
+    P: IdentifierPacket,
+    PP: PacketProcessor<P>,
+{
+    pub fn new(
+        identifer: &str,
+        connection: OwnedReadHalf,
+        processor: PP,
+        notify: Arc<Mutex<HashMap<String, oneshot::Sender<P>>>>,
+        capacity: usize,
+    ) -> Self {
+        Self {
+            identifer: identifer.to_string(),
+            inner: connection,
+            response_notify: notify,
+            processor,
+            buffer: BytesMut::with_capacity(capacity),
+        }
+    }
+
+    pub async fn run(&mut self, mut shutdown: broadcast::Receiver<()>) -> Result<()> {
+        loop {
+            let maybe_packet = tokio::select! {
+                res = self.read() => res?,
+                _ = shutdown.recv() => return Ok(())
+            };
+
+            let packet = match maybe_packet {
+                Some(packet) => packet,
+                None => return Ok(()),
+            };
+
+            let id = packet.id();
+            let notify = self.response_notify.lock().await.remove(id);
+            match notify {
+                Some(notify) => {
+                    if notify.send(packet).is_err() {
+                        // TODO maybe we should log the message's content
+                        logs::warn!("notify channel has closed, can not notify sender");
+                    }
+                }
+                None => self.processor.process(packet).await,
+            }
+        }
+    }
+
+    async fn read(&mut self) -> Result<Option<P>> {
+        loop {
+            let mut buf = Cursor::new(&self.buffer[..]);
+            if let Some(packet) = <P>::from_slice(&mut buf).await? {
+                let len = buf.position() as usize;
+                self.buffer.advance(len);
+                return Ok(Some(packet));
+            }
+
+            let size = self.inner.read_buf(&mut self.buffer).await?;
+            if size == 0 {
+                if self.buffer.is_empty() {
+                    return Ok(None);
+                } else {
+                    return Err(Error::Disconnect);
+                }
+            }
+            println!("server[{}] receive bytes: {}", self.identifer, size);
+        }
+    }
+}
+
+type NotifyMap<P> = Arc<Mutex<HashMap<String, Arc<Mutex<HashMap<String, oneshot::Sender<P>>>>>>>;
+#[derive(Clone)]
+pub struct BiProxy<P>
+where
+    P: IdentifierPacket,
+{
+    read_buffer_size: usize,
+    write_conns: Arc<Mutex<HashMap<String, WriteConnection>>>,
+    notify: NotifyMap<P>,
+    shutdown: broadcast::Sender<()>,
+    _a: std::marker::PhantomData<P>,
+}
+
+impl<P> BiProxy<P>
+where
+    P: IdentifierPacket,
+{
+    pub fn new(read_buffer_size: usize) -> Self {
+        Self {
+            read_buffer_size,
+            write_conns: Arc::new(Mutex::new(HashMap::new())),
+            notify: Arc::new(Mutex::new(HashMap::new())),
+            shutdown: broadcast::channel(1).0,
+            _a: std::marker::PhantomData::default(),
+        }
+    }
+
+    pub async fn accept_tcpstream<PP: PacketProcessor<P> + Clone>(
+        &self,
+        key: &str,
+        stream: TcpStream,
+        processor: PP,
+    ) {
+        let (read, write) = stream.into_split();
+        self.write_conns
+            .lock()
+            .await
+            .insert(key.to_string(), WriteConnection::new(write));
+
+        let notify_map = Arc::new(Mutex::new(HashMap::new()));
+        self.notify
+            .lock()
+            .await
+            .insert(key.to_string(), notify_map.clone());
+        self.spawn_read_task(key, notify_map, read, processor).await;
+    }
+
+    pub async fn send_oneway(&self, key: &str, data: P) -> Result<()> {
+        self.get_connection(key)
+            .await
+            .ok_or(Error::Disconnect)?
+            .write(data)
+            .await
+    }
+
+    pub async fn send(&self, key: &str, data: P) -> Result<P> {
+        let id = data.id().to_string();
+        let (sender, receiver) = oneshot::channel();
+        self.register_request_notify(key, id.as_str(), sender).await;
+
+        if let Err(error) = self.send_oneway(key, data).await {
+            self.deregister_request_notify(key, id.as_str()).await;
+            return Err(error);
+        }
+
+        let res = Response::new(receiver).await;
+        Ok(res)
+    }
+
+    async fn spawn_read_task<PP: PacketProcessor<P> + Clone>(
+        &self,
+        key: &str,
+        notify: Arc<Mutex<HashMap<String, oneshot::Sender<P>>>>,
+        conn: OwnedReadHalf,
+        processor: PP,
+    ) {
+        let mut read_conn =
+            ReadConnection::new(key, conn, processor, notify, self.read_buffer_size);
+        let shutdown = self.shutdown.subscribe();
+        tokio::spawn(async move {
+            if let Err(error) = read_conn.run(shutdown).await {
+                logs::error!("connection reset by peer, closed: {}", error);
+            }
+        });
+    }
+
+    async fn deregister_request_notify(&self, key: &str, packet_id: &str) {
+        let notify_map = self.notify.lock().await;
+        if let Some(notify_map) = notify_map.get(key) {
+            // ignore return
+            let _ = notify_map.lock().await.remove(packet_id);
+        }
+    }
+
+    async fn register_request_notify(
+        &self,
+        key: &str,
+        packet_id: &str,
+        sender: oneshot::Sender<P>,
+    ) {
+        let mut notify_map = self.notify.lock().await;
+
+        if !notify_map.contains_key(key) {
+            notify_map.insert(key.to_string(), Arc::new(Mutex::new(HashMap::new())));
+        }
+
+        let inner_notifys = match notify_map.get(key) {
+            Some(read_notify) => read_notify,
+            None => unreachable!(),
+        };
+
+        inner_notifys
+            .lock()
+            .await
+            .insert(packet_id.to_string(), sender);
+    }
+
+    async fn get_connection(&self, key: &str) -> Option<WriteConnection> {
+        self.write_conns.lock().await.get(key).cloned()
+    }
+}
+
+struct Response<P> {
+    notify: oneshot::Receiver<P>,
+}
+
+impl<P> Response<P> {
+    fn new(notify: oneshot::Receiver<P>) -> Self {
+        Self { notify }
+    }
+}
+
+impl<P> Future for Response<P> {
+    type Output = P;
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match Pin::new(&mut self.notify).poll(cx) {
+            Poll::Ready(Ok(value)) => Poll::Ready(value),
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(Err(_)) => unreachable!(),
+        }
+    }
+}
---
+++ tests/test.rs
@@ -0,0 +1,307 @@
+use std::{
+    fmt::Write,
+    io::{BufRead, Cursor},
+    sync::atomic::AtomicU64,
+    time::Duration,
+};
+
+use async_trait::async_trait;
+use biproxy::{BiProxy, Error, IdentifierPacket, PacketProcessor, Result};
+use bytes::{Buf, BytesMut};
+use concurrent_biproxy as biproxy;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+    sync::mpsc,
+};
+
+#[derive(Debug, Clone)]
+struct DefaultPacket {
+    id: String,
+    from: String,
+    target: String,
+    data: String,
+}
+
+#[async_trait]
+impl IdentifierPacket for DefaultPacket {
+    fn id(&self) -> &str {
+        self.id.as_str()
+    }
+    /// return Ok(None) if need more bytes
+    async fn from_slice(src: &mut Cursor<&[u8]>) -> Result<Option<Self>> {
+        if src.remaining() < 26 {
+            return Ok(None);
+        }
+        let mut id = String::new();
+        src.read_line(&mut id)?;
+
+        let mut from = String::new();
+        src.read_line(&mut from)?;
+
+        let mut target = String::new();
+        src.read_line(&mut target)?;
+
+        let mut data = String::new();
+        src.read_line(&mut data)?;
+
+        Ok(Some(Self {
+            id: id.as_str()[..id.len() - 1].to_string(),
+            from: from.as_str()[..from.len() - 1].to_string(),
+            target: target.as_str()[..target.len() - 1].to_string(),
+            data: data.as_str()[..data.len() - 1].to_string(),
+        }))
+    }
+    fn into_bytes(self) -> Vec<u8> {
+        let mut bytes = BytesMut::new();
+        bytes.write_str(self.id.as_str()).unwrap();
+        bytes.write_str("\n").unwrap();
+
+        bytes.write_str(self.from.as_str()).unwrap();
+        bytes.write_str("\n").unwrap();
+
+        bytes.write_str(self.target.as_str()).unwrap();
+        bytes.write_str("\n").unwrap();
+
+        bytes.write_str(self.data.as_str()).unwrap();
+        bytes.write_str("\n").unwrap();
+
+        bytes.to_vec()
+    }
+}
+
+#[derive(Clone)]
+struct DefaultProcessor;
+
+#[async_trait]
+impl PacketProcessor<DefaultPacket> for DefaultProcessor {
+    async fn process(&self, _: DefaultPacket) {}
+}
+
+struct Client {
+    identifier: String,
+    channel: mpsc::Sender<DefaultPacket>,
+}
+
+impl Client {
+    async fn new(identifier: &str, port: u16) -> Client {
+        let (wx, rx) = mpsc::channel(1);
+        let client = Self {
+            identifier: identifier.to_string(),
+            channel: wx,
+        };
+        let stream = tokio::net::TcpStream::connect(("0.0.0.0", port))
+            .await
+            .unwrap();
+        tokio::spawn(Self::event_loop(client.identifier.clone(), stream, rx));
+        client
+    }
+
+    async fn event_loop(
+        id: String,
+        mut stream: TcpStream,
+        mut channel: mpsc::Receiver<DefaultPacket>,
+    ) {
+        let mut buffer = BytesMut::new();
+
+        loop {
+            tokio::select! {
+                send_data = channel.recv() => {
+                    let packet = match send_data {
+                        Some(packet) => packet,
+                        None => break
+                    };
+                    let bytes = packet.into_bytes();
+                    println!("client[{}] send total bytes: {}", id, bytes.len());
+                    stream.write_all(bytes.as_slice()).await.unwrap();
+                },
+                packet = Self::read_packet(id.as_str(), &mut stream, &mut buffer) => {
+                    // let resp = DefaultPacket {
+                    //     id: packet.id.clone(),
+                    //     from: packet.target.clone(),
+                    //     target: packet.from.clone(),
+                    //     data: "world".to_string()
+                    // };
+                    // let bytes = resp.into_bytes();
+                    // stream.write_all(bytes.as_slice()).await.unwrap();
+                    assert_eq!(id, packet.target, "client_id:{} compare packet's target: {}", id, packet.target);
+                }
+            }
+        }
+    }
+
+    async fn send(&self, data: DefaultPacket) {
+        self.channel.send(data).await.unwrap();
+    }
+
+    async fn read_packet(id: &str, stream: &mut TcpStream, buffer: &mut BytesMut) -> DefaultPacket {
+        match Self::read(id, stream, buffer).await {
+            Ok(Some(packet)) => return packet,
+            _ => {
+                panic!("connection reset by peer")
+            }
+        }
+    }
+
+    async fn read(
+        id: &str,
+        stream: &mut TcpStream,
+        buffer: &mut BytesMut,
+    ) -> Result<Option<DefaultPacket>> {
+        loop {
+            let mut buf = Cursor::new(&buffer[..]);
+            if let Some(packet) = DefaultPacket::from_slice(&mut buf).await? {
+                let len = buf.position() as usize;
+                buffer.advance(len);
+                return Ok(Some(packet));
+            }
+
+            let size = stream.read_buf(buffer).await?;
+            if 0 == size {
+                if buffer.is_empty() {
+                    return Ok(None);
+                } else {
+                    return Err(Error::Disconnect);
+                }
+            }
+            println!("client[{}] receive bytes: {}", id, size)
+        }
+    }
+}
+
+async fn init_proxy() -> BiProxy<DefaultPacket> {
+    let proxy = BiProxy::new(2048);
+    let proxy_clone = proxy.clone();
+    let processor = LocalRouterProcessor {
+        proxy: proxy.clone(),
+    };
+    tokio::spawn(async move {
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+        let mut counter = 1;
+        loop {
+            if let Ok((socket, _)) = listener.accept().await {
+                proxy_clone
+                    .accept_tcpstream(
+                        format!("user{}", counter).as_str(),
+                        socket,
+                        processor.clone(),
+                    )
+                    .await;
+                counter += 1;
+            }
+        }
+    });
+    tokio::time::sleep(Duration::from_millis(100)).await;
+    proxy
+}
+
+#[derive(Clone)]
+struct LocalRouterProcessor {
+    proxy: BiProxy<DefaultPacket>,
+}
+
+#[async_trait]
+impl PacketProcessor<DefaultPacket> for LocalRouterProcessor {
+    async fn process(&self, packet: DefaultPacket) {
+        let target = packet.target.clone();
+        let _ = self.proxy.send_oneway(target.as_str(), packet).await;
+    }
+}
+
+#[tokio::test]
+pub async fn tset_c2c() -> Result<()> {
+    let _proxy = init_proxy().await;
+    let client1 = Client::new(format!("user{}", 1).as_str(), 3000).await;
+    let client2 = Client::new(format!("user{}", 2).as_str(), 3000).await;
+
+    let request = DefaultPacket {
+        id: "xasdfa1".to_string(),
+        from: "user1".to_string(),
+        target: "user2".to_string(),
+        data: "hello".to_string(),
+    };
+
+    client1.send(request).await;
+    let request = DefaultPacket {
+        id: "xasdfa2".to_string(),
+        from: "user2".to_string(),
+        target: "user1".to_string(),
+        data: "hello".to_string(),
+    };
+    client2.send(request).await;
+    tokio::time::sleep(Duration::from_millis(10)).await;
+    Ok(())
+}
+
+#[tokio::test]
+pub async fn test_sync_send() -> Result<()> {
+    let proxy = init_proxy().await;
+    let _client = Client::new(format!("user{}", 1).as_str(), 3000).await;
+    let request = DefaultPacket {
+        id: "xasdfas".to_string(),
+        from: "user2".to_string(),
+        target: "user1".to_string(),
+        data: "hello".to_string(),
+    };
+    tokio::time::sleep(Duration::from_millis(100)).await;
+    let res = proxy.send("user1", request).await.unwrap();
+    println!("{:?}", res);
+    assert_eq!(res.target, "user2".to_string());
+    assert_eq!(res.data, "world".to_string());
+    Ok(())
+}
+
+#[tokio::test]
+pub async fn test_async_send() -> Result<()> {
+    let proxy = init_proxy().await;
+    let _client = Client::new(format!("user{}", 1).as_str(), 3000).await;
+    let request = DefaultPacket {
+        id: "xasdfas".to_string(),
+        from: "user2".to_string(),
+        target: "user1".to_string(),
+        data: "hello".to_string(),
+    };
+    tokio::time::sleep(Duration::from_millis(100)).await;
+    let _ = proxy.send_oneway("user1", request).await.unwrap();
+    tokio::time::sleep(Duration::from_millis(100)).await;
+    Ok(())
+}
+
+#[tokio::test]
+pub async fn test_bench_send() -> Result<()> {
+    let proxy = init_proxy().await;
+
+    let mut clients = vec![];
+    let client_num = 1000;
+    for id in 1..=client_num {
+        clients.push(Client::new(format!("user{}", id).as_str(), 3000).await)
+    }
+
+    let counter = AtomicU64::new(0);
+
+    let mut message_counter = 1_000_000;
+    while message_counter > 0 {
+        let target = format!("user{}", rand::random::<u32>() % client_num + 1);
+        let packet = DefaultPacket {
+            id: format!(
+                "{}",
+                counter.fetch_add(1, std::sync::atomic::Ordering::SeqCst)
+            ),
+            from: format!("user{}", rand::random::<u32>() % 999 + 1),
+            target: target.clone(),
+            data: "hello".to_string(),
+        };
+
+        let new_proxy = proxy.clone();
+        tokio::spawn(async move {
+            if let Err(error) = new_proxy.send_oneway(target.as_str(), packet).await {
+                println!("can not send to[{}]:{}", target, error);
+            }
+        });
+
+        message_counter -= 1;
+    }
+    tokio::time::sleep(Duration::from_secs(60)).await;
+    Ok(())
+}
